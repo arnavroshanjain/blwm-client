7d12cc95e03bfda09872004f8d11c9853ad78d8a
Arnav Jain <JainAR1@cardiff.ac.uk>
2022-11-25 13:26:11 +0000

    Fixed Javascript

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,6 +2,7 @@ function login_function() {
   var email = document.getElementById('exampleInputEmail1').value;
   var password = document.getElementById('exampleInputPassword1').value;
   var params = 'email='+email+'&password='+password;
+  window.alert(response)
   var xhttp = new XMLHttpRequest();
   xhttp.open('POST', 'login_request', true);
   xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -9,8 +10,9 @@ function login_function() {
       if (xhttp.readyState === 4 && xhttp.status === 200) {
         var response = xhttp.responseText;#
 
-        window.alert(response)
+// fix error here
 
+//insert stuff
         }
       } else {
         console.error(xhttp.statusText);
