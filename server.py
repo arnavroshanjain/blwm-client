87d28fa7f18866f797278479620451e2fc671ad7
Abir Chowdhury <chowdhuryah1@cardiff.ac.uk>
2022-11-25 17:34:24 +0000

    contanct page linked to database

--- server.py
+++ server.py
@@ -3,36 +3,35 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = sqlite3.connect('blwmDB.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+	conn = sqlite3.connect('blwmDB.db')
+	conn.row_factory = sqlite3.Row
+	return conn
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+	return render_template('homepage.html')
+
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+	return render_template('contact.html')
 
 
-@app.route('/contact', methods=['POST','GET'])
-def login_request():
+@app.route('/contact_request', methods=['POST','GET'])
+def contact_request():
 
-
-
-   if request.method == 'POST':
-        first_name = request.form['FirstName']
-        last_Name = request.form['LastName']
-        email = request.form['Email']
-        number = request.form['Numbers']
-        comment = request.form['Comments']
-        
-    conn = get_db_connection()
-    conn.execute('INSERT INTO tbl_contact (first_name, last_name, email, phone_number, comments) VALUES (?, ?, ?, ?, ?)',
-    (FirstName, LastName, Email, Numbers, Comments))
-    conn.commit()
-    conn.close()
-    return 'true'
-    
+	if request.method == 'POST':
+		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		email = request.form['email']
+		number = request.form['number']
+		comment = request.form['comment']
+  
+	conn = get_db_connection()
+	conn.execute('INSERT INTO tbl_contact (first_name, last_name, email, number, comment) VALUES (?, ?, ?, ?, ?)',
+	(first_name, last_name, email, number, comment))
+	conn.commit()
+	conn.close()
+	return 'true'
+	
 if __name__ == "__main__":
- app.run(debug=True)
+	app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,43 +1,25 @@
 function contact() {
-
-    var first_name = document.getElementById('FirstName').value;
-    var Last_name = document.getElementById('LastName').value;
-    var email = document.getElementById('Email').value;
-    var number = document.getElementById('Numbers').value;
-    var comment = document.getElementById('Comments').value;
-    
-
+    var first_name = document.getElementById('first_name').value;
+    var last_name = document.getElementById('last_name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var comment = document.getElementById('comment').value;
   
-    var params = 'first_name='+FirstName+ '&last_name'+LastName+'&email'+Email+'&number'+Numbers+'&comment'+Comments;
-  
+    var params = 'first_name='+first_name+'&last_name='+last_name+'&email='+email+'&number='+number+'&comment='+comment;
     var xhttp = new XMLHttpRequest();
-  
-    xhttp.open('POST', 'login_request', true);
-  
+    xhttp.open('POST', 'contact_request', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  
       xhttp.onreadystatechange = function() {
-  
         if (xhttp.readyState === 4 && xhttp.status === 200) {
-  
           var response = xhttp.responseText;
-  
           if (response == 'True') {
-  
             window.location.replace('../');
-  
           } else {
-  
-            document.getElementById('error_output').innerHTML = response;
-  
+            window.alert(response)
           }
-  
         } else {
-  
           console.error(xhttp.statusText);
-  
         }
-  
       };
   
     xhttp.send(params);
