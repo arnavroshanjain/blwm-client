f71228cf5d3ae208111bebf8b1c4363abdc5a411
Oran Kendrew <kendrewo@cardiff.ac.uk>
2022-11-28 14:35:12 +0000

    cleaned up some code and added some style to the login page

--- server.py
+++ server.py
@@ -17,7 +17,6 @@ def create_school():
 
 @app.route('/register/school_request', methods=['POST', 'GET'])
 def create_school_request():
-    
     if request.method == 'POST': 
         name = request.form['name']
         address = request.form['address']
@@ -25,14 +24,12 @@ def create_school_request():
         phone_number = request.form['phone_number']
         # logo = request.files['logo']
         website = request.form['website']
-
         conn = get_db_connection()
         conn.execute('INSERT INTO tbl_schools (school_name, school_address, school_logo, school_email, school_phone_number, school_website,creator_user_id) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        (name, address, 'logo', email, phone_number, website, 1))
+        (name, address, 'logo', email, phone_number, website, session['login']))
         conn.commit()
         conn.close()
         return 'True'
-
     return 'Failed to create school, please try again.'
 
 if __name__ == "__main__":
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,28 +1,22 @@
 function create_school_request() {
-
     var name = document.getElementById('school_name').value;
     var address = document.getElementById('school_address').value;
     var email = document.getElementById('school_email').value;
     var phone_number = document.getElementById('school_phone_number').value;
     var logo = document.getElementById('school_logo').value;
     var website = document.getElementById('school_website').value;
-    var params = 'name='+name+'&address='+address+'&email='+email+'&phone_number='+phone_number+'&logo='+logo+'&website='+website;
-
-    window.alert(params);
-    
+    var params = 'name='+name+'&address='+address+'&email='+email+'&phone_number='+phone_number+'&logo='+logo+'&website='+website;    
     var xhttp = new XMLHttpRequest();
     xhttp.open('POST', 'school_request', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
-
           var response = xhttp.responseText;
           if (response == 'True') {
             window.location.replace('../');
           } else {
             document.getElementById('error_output').innerHTML = response;
           }
-
         } else {
           console.error(xhttp.statusText);
         }
